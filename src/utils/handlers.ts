e5fa3fcb18dd75ae511174fedc36c8d37f5bff0f
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-07-12 22:19:40 +0700

    update

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -1,5 +1,5 @@
 import Express from 'express'
-export default function wrapRequestHandler (Func: Express.RequestHandler) {
+export default function wrapRequestHandler(Func: Express.RequestHandler) {
   return async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     try {
       await Func(req, res, next)
