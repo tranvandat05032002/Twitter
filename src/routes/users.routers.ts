4b67f79a935663cd36deb87825e7db35dc853403
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-07-13 21:44:25 +0700

    update

--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -12,5 +12,11 @@ const usersRouter = Router()
  * Body: { email: string, password: string }
  */
 usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
+/**
+ * Description. Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
+ */
 usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 export default usersRouter
