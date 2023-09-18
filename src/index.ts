5a2bcb0c5d78f0a1fa6d44e0f6c1820a4ca6b0ff
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-09-18 17:02:39 +0700

    Upload single image with formidable

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import usersRouter from './routes/users.routers'
 import databaseService from './services/database.services'
 import { defaultHandleError } from './middlewares/errors.middlewares'
 import cors from 'cors'
+import mediasRouter from './routes/medias.routers'
+import { initFolder } from './utils/file'
 dotenv.config()
 const app = express()
 // app.use(cors())
@@ -18,8 +20,10 @@ databaseService.connect()
 const PORT = process.env.PORT
 const DOMAIN = process.env.DOMAIN
 
+initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultHandleError)
 
 app.listen(PORT, () => {
---
+++ src/utils/file.ts
@@ -0,0 +1,40 @@
+import { Request } from 'express'
+import fs from 'fs'
+import path, { resolve } from 'path'
+export const initFolder = () => {
+  const uploadFolderFile = path.resolve('uploads')
+  if (!fs.existsSync(uploadFolderFile)) {
+    fs.mkdirSync(uploadFolderFile, {
+      recursive: true // create folder nested
+    })
+  }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFiles: 1,
+    maxFileSize: 500 * 1024, // 500KB
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not value') as any)
+      }
+      return true
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
+}
