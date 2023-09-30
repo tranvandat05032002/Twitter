3f14392d41508e276aabd1b2bd6d02399195a1c1
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-09-30 19:30:09 +0700

    fix name folder orther to other

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -23,3 +23,14 @@ export enum EncodingStatus {
   success,
   failed
 }
+
+export enum TweetType {
+  Tweet,
+  Retweet,
+  Comment,
+  QuoteTweet
+}
+export enum TweetAudience {
+  Everyone, // 0
+  TwitterCircle // 1
+}
