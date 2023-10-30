a4ce3cb8c81904a954db4733853af5c66ed71c87
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-10-30 12:53:26 +0700

    using @aws-sdk/lib-storage upload file to s3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,8 @@
 import { config } from 'dotenv'
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import fs from 'fs'
+import path from 'path'
 config()
 const s3 = new S3({
   region: process.env.AWS_REGION as string,
@@ -10,4 +13,23 @@ const s3 = new S3({
 })
 s3.listBuckets({}).then((data) => {
   console.log(data)
+})
+const file = fs.readFileSync(path.resolve('uploads/image/image_2023-09-25_10-59-44.png'))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'twitter-s3-ap-southeast-1', Key: 'image2.jpg', Body: file, ContentType: 'image/jpeg' },
+
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+parallelUploads3.done().then((res) => {
+  console.log(res)
 })
